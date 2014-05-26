702841fde6b760ea2647698fce9004d527ba31bd
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2014-05-26 20:05:50 +0000

    Added a meta model for PCM based measuring points
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/EDP2/trunk@22625 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.edp2.edit/src/org/palladiosimulator/edp2/models/ExperimentData/provider/EDP2EditPlugin.java
+++ org.palladiosimulator.edp2.edit/src/org/palladiosimulator/edp2/models/ExperimentData/provider/EDP2EditPlugin.java
@@ -11,10 +11,6 @@ import org.eclipse.emf.common.util.ResourceLocator;
 import org.palladiosimulator.metricspec.provider.MetricSpecEditPlugin;
 
 import de.uka.ipd.sdq.identifier.provider.IdentifierEditPlugin;
-import de.uka.ipd.sdq.pcm.core.provider.PalladioComponentModelEditPlugin;
-import de.uka.ipd.sdq.probfunction.provider.ProbabilityFunctionEditPlugin;
-import de.uka.ipd.sdq.stoex.provider.StoexEditPlugin;
-import de.uka.ipd.sdq.units.provider.UnitsEditPlugin;
 
 /**
  * This is the central singleton for the EDP2 edit plugin.
@@ -50,10 +46,6 @@ public final class EDP2EditPlugin extends EMFPlugin {
           (new ResourceLocator [] {
              IdentifierEditPlugin.INSTANCE,
              MetricSpecEditPlugin.INSTANCE,
-             PalladioComponentModelEditPlugin.INSTANCE,
-             ProbabilityFunctionEditPlugin.INSTANCE,
-             StoexEditPlugin.INSTANCE,
-             UnitsEditPlugin.INSTANCE,
            });
     }
 

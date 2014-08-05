00a22fbb232f8619ccee27d5f6db91145d8e1eb0
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2014-08-05 17:05:42 +0000

    Adapter now supports filtering-out unwanted elements.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/EDP2/trunk@23871 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.edp2.datastream/src/org/palladiosimulator/edp2/datastream/filter/AbstractAdapter.java
+++ org.palladiosimulator.edp2.datastream/src/org/palladiosimulator/edp2/datastream/filter/AbstractAdapter.java
@@ -14,6 +14,108 @@ import org.palladiosimulator.metricspec.MetricDescription;
 
 public abstract class AbstractAdapter extends AbstractDataSource implements IDataSink, IDataSource {
 
+    private final class AdapterDataStream implements IDataStream<Measurement> {
+
+        private final class AdapterIterator implements Iterator<Measurement> {
+
+            private Measurement next;
+            private Measurement nextNext;
+            private int count = 0;
+
+            private final Iterator<Measurement> inputIterator;
+
+            private AdapterIterator(Iterator<Measurement> inputIterator) {
+                this.inputIterator = inputIterator;
+
+                this.next = findNext();
+                this.nextNext = findNext();
+            }
+
+            @Override
+            public boolean hasNext() {
+                return this.next != null;
+            }
+
+            @Override
+            public Measurement next() {
+                final Measurement measurement = AbstractAdapter.this.computeOutputFromInput(this.next);
+
+                if (!isCompatibleWith(measurement.getMetricDesciption())) {
+                    throw new RuntimeException("Metric \"" + measurement.getMetricDesciption().getName()
+                            + "\" is incompatible with expected output metric \""
+                            + AbstractAdapter.this.getMetricDesciption().getName() + "\"");
+                }
+
+                this.next = this.nextNext;
+                this.nextNext = findNext();
+
+                count++;
+                if (!hasNext()) {
+                    AdapterDataStream.this.cachedSize = count;
+                }
+                return measurement;
+            }
+
+            @Override
+            public void remove() {
+            }
+
+            private Measurement findNext() {
+                Measurement result = null;
+
+                while (inputIterator.hasNext() && AbstractAdapter.this.shouldSkip(result = inputIterator.next())) {
+                    ;
+                }
+
+                return inputIterator.hasNext() ? result : null;
+            }
+        }
+
+        private int cachedSize = -1;
+
+        private final IDataStream<Measurement> inputDataStream;
+
+        private AdapterDataStream(final IDataStream<Measurement> inputDataStream) {
+            this.inputDataStream = inputDataStream;
+        }
+
+        @Override
+        public Iterator<Measurement> iterator() {
+            return new AdapterIterator(this.inputDataStream.iterator());
+        }
+
+        @Override
+        public MetricDescription getMetricDesciption() {
+            return AbstractAdapter.this.getMetricDesciption();
+        }
+
+        @Override
+        public boolean isCompatibleWith(final MetricDescription other) {
+            return AbstractAdapter.this.isCompatibleWith(other);
+        }
+
+        @Override
+        public void close() {
+            inputDataStream.close();
+        }
+
+        @Override
+        public int size() {
+            if (this.cachedSize < 0) {
+
+                this.cachedSize = 0;
+
+                if (this.inputDataStream.size() != 0) {
+                    for (final Measurement measurement : this) {
+                        this.cachedSize++;
+                    }
+                }
+            }
+
+            return this.cachedSize;
+        }
+    }
+
     protected final IDataStream<Measurement> inputDataStream;
     private final IDataSource datasource;
 
@@ -35,52 +137,16 @@ public abstract class AbstractAdapter extends AbstractDataSource implements IDat
      */
     @Override
     public IDataStream<Measurement> getDataStream() {
-        final Iterator<Measurement> inputIterator = this.inputDataStream.iterator();
-        return new IDataStream<Measurement>() {
-
-            @Override
-            public Iterator<Measurement> iterator() {
-                return new Iterator<Measurement>() {
-
-                    @Override
-                    public boolean hasNext() {
-                        return inputIterator.hasNext();
-                    }
-
-                    @Override
-                    public Measurement next() {
-                        return AbstractAdapter.this.computeOutputFromInput(inputIterator.next());
-                    }
-
-                    @Override
-                    public void remove() {
-                    }
-                };
-            }
-
-            @Override
-            public MetricDescription getMetricDesciption() {
-                return AbstractAdapter.this.getMetricDesciption();
-            }
-
-            @Override
-            public boolean isCompatibleWith(final MetricDescription other) {
-                return AbstractAdapter.this.isCompatibleWith(other);
-            }
-
-            @Override
-            public void close() {
-                inputDataStream.close();
-            }
-
-            @Override
-            public int size() {
-                return inputDataStream.size();
-            }
-        };
+        return new AdapterDataStream(this.inputDataStream);
     }
 
-    protected abstract Measurement computeOutputFromInput(Measurement next);
+    protected boolean shouldSkip(Measurement next) {
+        return false;
+    }
+
+    protected Measurement computeOutputFromInput(Measurement next) {
+        return next;
+    }
 
     /*
      * (non-Javadoc)

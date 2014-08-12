f49cc44ed1d8f912d6122308401d89886e1c74e2
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2014-08-12 12:22:02 +0000

    Improved EDP2 perspective (added properties and problems view; added launch tool bar)
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/EDP2/trunk@23988 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.edp2.ui/src/org/palladiosimulator/edp2/ui/perspective/Perspective.java
+++ org.palladiosimulator.edp2.ui/src/org/palladiosimulator/edp2/ui/perspective/Perspective.java
@@ -1,5 +1,6 @@
 package org.palladiosimulator.edp2.ui.perspective;
 
+import org.eclipse.debug.ui.IDebugUIConstants;
 import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
@@ -10,16 +11,33 @@ import org.eclipse.ui.IPerspectiveFactory;
  * @author Sebastian Lehrig
  */
 public class Perspective implements IPerspectiveFactory {
+
     private final String NAVIGATOR_ID = "org.palladiosimulator.edp2.ui.views.navigator";
+
+    private final float GOLDEN_RATIO = 0.618f;
 
     @Override
     public void createInitialLayout(IPageLayout layout) {
-        String editorArea = layout.getEditorArea();
+        // basic layout settings
+        final String editorArea = layout.getEditorArea();
         layout.setEditorAreaVisible(true);
+        layout.setFixed(false);
+        final IFolderLayout topLeft = layout.createFolder("topLeft", IPageLayout.LEFT, 1.0f - GOLDEN_RATIO, editorArea);
+        final IFolderLayout bottom = layout.createFolder("bottom", IPageLayout.BOTTOM, GOLDEN_RATIO, editorArea);
 
-        IFolderLayout navFolder = layout.createFolder("experiments", IPageLayout.LEFT, 0.33f, editorArea);
-        navFolder.addView(NAVIGATOR_ID);
+        // EDP2 (tree) navigator view
+        topLeft.addView(NAVIGATOR_ID);
         layout.getViewLayout(NAVIGATOR_ID).setCloseable(false);
+
+        // properties view
+        bottom.addView(IPageLayout.ID_PROP_SHEET);
+        layout.getViewLayout(IPageLayout.ID_PROP_SHEET).setCloseable(false);
+
+        // problems view
+        bottom.addView(IPageLayout.ID_PROBLEM_VIEW);
+
+        // enable debug/launch buttons in tool bar
+        layout.addActionSet(IDebugUIConstants.LAUNCH_ACTION_SET);
     }
 
 }

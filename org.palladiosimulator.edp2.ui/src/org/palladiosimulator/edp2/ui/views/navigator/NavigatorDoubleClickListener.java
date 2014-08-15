775a96ca41200ec166abaea5c765f91071a483e0
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2014-08-15 12:51:56 +0000

    Refactored into methods
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/EDP2/trunk@24099 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.edp2.ui/src/org/palladiosimulator/edp2/ui/views/navigator/NavigatorDoubleClickListener.java
+++ org.palladiosimulator.edp2.ui/src/org/palladiosimulator/edp2/ui/views/navigator/NavigatorDoubleClickListener.java
@@ -8,12 +8,12 @@ import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.palladiosimulator.edp2.datastream.IDataSource;
+import org.palladiosimulator.edp2.datastream.chaindescription.ChainDescription;
 import org.palladiosimulator.edp2.datastream.edp2source.Edp2DataTupleDataSource;
 import org.palladiosimulator.edp2.models.ExperimentData.Measurements;
 import org.palladiosimulator.edp2.models.ExperimentData.RawMeasurements;
 import org.palladiosimulator.edp2.ui.EDP2UIPlugin;
 import org.palladiosimulator.edp2.visualization.IVisualisationInput;
-import org.palladiosimulator.edp2.visualization.wizards.DefaultSequence;
 import org.palladiosimulator.edp2.visualization.wizards.DefaultViewsWizard;
 
 /**
@@ -21,7 +21,7 @@ import org.palladiosimulator.edp2.visualization.wizards.DefaultViewsWizard;
  * associated with the selected {@link RawMeasurements}. Upon Double-click on a
  * {@link RawMeasurements} -object, it opens a Dialog and offers possible combinations of
  * visualizations and transformations to display the data encapsulated by the object. All
- * combinations are objects of the Type {@link DefaultSequence} and displayed in the
+ * combinations are objects of the Type {@link ChainDescription} and displayed in the
  * {@link DefaultViewsWizard}.
  *
  * TODO This is a copied version of /org.palladiosimulator.edp2.editor/src-man/de/uka
@@ -40,45 +40,61 @@ public class NavigatorDoubleClickListener implements IDoubleClickListener {
             final IStructuredSelection selection = (IStructuredSelection) event.getSelection();
             selectedObject = selection.getFirstElement();
         }
-
         // check for the object to contain actual data
         if (selectedObject instanceof Measurements) {
-
-            final Measurements measurements = (Measurements) selectedObject;
-            final RawMeasurements rawMeasurements = measurements.getMeasurementsRanges().get(0).getRawMeasurements();
-
-            if (rawMeasurements != null && !rawMeasurements.getDataSeries().isEmpty()) {
-                final IDataSource edp2Source = new Edp2DataTupleDataSource(rawMeasurements);
-                final int dataStreamSize = edp2Source.getDataStream().size();
-                edp2Source.getDataStream().close();
-
-                if (dataStreamSize > 0) {
-                    // open the wizard with reference to the selected source
-                    // it shows possible visualizations, which are instances of
-                    // DefaultSequence
-                    final DefaultViewsWizard wizard = new DefaultViewsWizard(edp2Source);
-                    final WizardDialog wdialog = new WizardDialog(EDP2UIPlugin.INSTANCE.getWorkbench()
-                            .getActiveWorkbenchWindow().getShell(), wizard);
-                    wdialog.open();
-
-                    if (wdialog.getReturnCode() == Window.OK) {
-                        final DefaultSequence selection = wizard.getSelectedDefault();
-                        final IVisualisationInput input = selection.getVisualizationInput();
-                        input.addInput(input.createNewInput(edp2Source));
-                        try {
-                            final IWorkbenchPage page = EDP2UIPlugin.INSTANCE.getWorkbench().getActiveWorkbenchWindow()
-                                    .getActivePage();
-                            page.openEditor(input, "org.palladiosimulator.edp2.visualization.editors.JFreeChartEditor");
-                        } catch (final PartInitException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                }
-            } else {
-                throw new RuntimeException("Empty Measurements!");
-            }
+            openChainSelectionDialog(selectedObject);
         }
+    }
 
+    /**
+     * @param selectedObject
+     */
+    private void openChainSelectionDialog(final Object selectedObject) {
+        final Measurements measurements = (Measurements) selectedObject;
+        final RawMeasurements rawMeasurements = measurements.getMeasurementsRanges().get(0).getRawMeasurements();
+
+        if (rawMeasurements != null && !rawMeasurements.getDataSeries().isEmpty()) {
+            final IDataSource edp2Source = new Edp2DataTupleDataSource(rawMeasurements);
+            final int dataStreamSize = edp2Source.getDataStream().size();
+            edp2Source.getDataStream().close();
+
+            if (dataStreamSize > 0) {
+                openWizard(edp2Source);
+            }
+        } else {
+            throw new RuntimeException("Empty Measurements!");
+        }
+    }
+
+    // open the wizard with reference to the selected source
+    // it shows possible visualizations, which are instances of
+    // DefaultSequence
+    private void openWizard(final IDataSource edp2Source) {
+        final DefaultViewsWizard wizard = new DefaultViewsWizard(edp2Source);
+        final WizardDialog wdialog = new WizardDialog(EDP2UIPlugin.INSTANCE.getWorkbench()
+                .getActiveWorkbenchWindow().getShell(), wizard);
+        wdialog.open();
+
+        if (wdialog.getReturnCode() == Window.OK) {
+            openEditor(edp2Source, wizard);
+        }
+    }
+
+    /**
+     * @param edp2Source
+     * @param wizard
+     */
+    private void openEditor(final IDataSource edp2Source, final DefaultViewsWizard wizard) {
+        final ChainDescription chainDescription = wizard.getSelectedDefault();
+        final IVisualisationInput input = (IVisualisationInput) chainDescription.getVisualizationInput();
+        input.addInput(input.createNewInput(chainDescription.attachRootDataSource(edp2Source)));
+        try {
+            final IWorkbenchPage page = EDP2UIPlugin.INSTANCE.getWorkbench().getActiveWorkbenchWindow()
+                    .getActivePage();
+            page.openEditor(input, "org.palladiosimulator.edp2.visualization.editors.JFreeChartEditor");
+        } catch (final PartInitException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }

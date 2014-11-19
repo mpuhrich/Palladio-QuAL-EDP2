f47a596d2ec45cbb48b467e54d2ec369644394b2
florian-rosenthal <florian.rosenthal@kit.edu>
2014-11-19 14:06:37 +0000

    add "Export Data as CSV..." to additional functions context menu of JFreeCharts
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/EDP2/trunk@24982 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.edp2.visualization.jfreechart/src/org/palladiosimulator/edp2/visualization/jfreechart/editor/CustomJFreeChartComposite.java
+++ org.palladiosimulator.edp2.visualization.jfreechart/src/org/palladiosimulator/edp2/visualization/jfreechart/editor/CustomJFreeChartComposite.java
@@ -8,11 +8,14 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.apache.batik.dom.GenericDOMImplementation;
 import org.apache.batik.svggen.SVGGraphics2D;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.FileDialog;
@@ -20,6 +23,8 @@ import org.eclipse.swt.widgets.Menu;
 import org.eclipse.ui.PlatformUI;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.xy.XYDataset;
 import org.jfree.experimental.chart.swt.ChartComposite;
 import org.w3c.dom.DOMImplementation;
 import org.w3c.dom.Document;
@@ -28,20 +33,18 @@ public class CustomJFreeChartComposite extends ChartComposite {
 
     private File selectFileDialog(final String filter) {
         final FileDialog dialog = new FileDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell());
-        dialog.setFilterExtensions(new String[] {
-            filter
-        });
-        dialog.setText("Enter the image file name");
+        dialog.setFilterExtensions(new String[] { filter });
+        dialog.setText("Enter the file name");
         final String result = dialog.open();
         if (result != null) {
-            final String filename = dialog.getFilterPath() + File.separator + dialog.getFileName();
+            String filename = dialog.getFilterPath() + File.separator + dialog.getFileName();
             return new File(filename);
         } else {
             return null;
         }
     }
 
-    class SaveImageAsAction extends Action {
+    private class SaveImageAsAction extends Action {
 
         public SaveImageAsAction() {
             super();
@@ -61,7 +64,7 @@ public class CustomJFreeChartComposite extends ChartComposite {
         }
     }
 
-    class SaveSVGAsAction extends Action {
+    private class SaveSVGAsAction extends Action {
 
         public SaveSVGAsAction() {
             super();
@@ -70,7 +73,7 @@ public class CustomJFreeChartComposite extends ChartComposite {
 
         @Override
         public void run() {
-            final File f = selectFileDialog("*.png");
+            final File f = selectFileDialog("*.svg");
             if (f != null) {
                 try {
                     final JFreeChart chart = getChart();
@@ -86,6 +89,41 @@ public class CustomJFreeChartComposite extends ChartComposite {
                     out.close();
                 } catch (final IOException e) {
                     throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    private class ExportCsvAsAction extends Action {
+
+        private static final String ONE_LINE_FORMAT = "%s,%s";
+
+        public ExportCsvAsAction() {
+            super();
+            setText("Export Data as CSV...");
+        }
+        
+        public void run() {
+            File f = selectFileDialog("*.csv");
+            if (f != null) {
+                XYPlot plot = getChart().getXYPlot();
+                XYDataset dataset = plot.getDataset();
+                StringBuilder content = new StringBuilder(String.format(ONE_LINE_FORMAT, plot.getDomainAxis().getLabel(), plot.getRangeAxis()
+                        .getLabel()));
+                // assume that we export the last (and only) data series
+                int seriesNumber = dataset.getSeriesCount() - 1;
+                for (int i = 0; i < dataset.getItemCount(seriesNumber); ++i) {
+                    content.append(System.lineSeparator());
+                    double x = dataset.getXValue(seriesNumber, i);
+                    double y = dataset.getYValue(seriesNumber, i);
+                    content.append(String.format(ONE_LINE_FORMAT, x, y));
+                }
+                try {
+                    Files.write(Paths.get(f.getPath()), content.toString().getBytes());
+                } catch (IOException e) {
+                    StringBuilder errorMessage = new StringBuilder("An error occurred during CSV export: "+ System.lineSeparator());
+                    errorMessage.append(e.getMessage());
+                    MessageDialog.openError(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), "Error", errorMessage.toString());
                 }
             }
         }
@@ -107,5 +145,6 @@ public class CustomJFreeChartComposite extends ChartComposite {
     protected void initializeContextMenu(final MenuManager menu_manager) {
         menu_manager.add(new SaveImageAsAction());
         menu_manager.add(new SaveSVGAsAction());
+        menu_manager.add(new ExportCsvAsAction());
     }
 }

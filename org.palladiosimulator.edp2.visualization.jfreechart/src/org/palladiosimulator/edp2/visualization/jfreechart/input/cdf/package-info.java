854aefb11402138edc869e65f3cc5951fdf7756d
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2015-06-19 17:50:52 +0000

    Added package for EDP-11
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/EDP2/trunk@27379 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ org.palladiosimulator.edp2.visualization.jfreechart/src/org/palladiosimulator/edp2/visualization/jfreechart/input/cdf/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Visualization for Cumulative Distribution Functions (CDFs)
+ * 
+ * TODO Implement this visualization
+ * 
+ * @see https://en.wikipedia.org/wiki/Cumulative_distribution_function
+ * 
+ * @author Sebastian Lehrig
+ */
+package org.palladiosimulator.edp2.visualization.jfreechart.input.cdf;

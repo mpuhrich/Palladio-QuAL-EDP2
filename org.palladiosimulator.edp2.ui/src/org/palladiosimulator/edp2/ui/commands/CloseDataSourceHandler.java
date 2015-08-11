334939c5ae4a03e0fa4e61d203dc084fc027571c
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2015-08-11 11:20:11 +0000

    Fixed EDP2-28
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/EDP2/trunk@28754 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.edp2.ui/src/org/palladiosimulator/edp2/ui/commands/CloseDataSourceHandler.java
+++ org.palladiosimulator.edp2.ui/src/org/palladiosimulator/edp2/ui/commands/CloseDataSourceHandler.java
@@ -4,17 +4,22 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.TreeSelection;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.palladiosimulator.edp2.EDP2Plugin;
 import org.palladiosimulator.edp2.dao.exception.DataNotAccessibleException;
 import org.palladiosimulator.edp2.models.Repository.Repository;
+import org.palladiosimulator.edp2.ui.EDP2UIPlugin;
 import org.palladiosimulator.edp2.util.MeasurementsUtility;
 
 /**
  * Command for opening the OpenDataSourceWizard.
- * 
+ *
  * @author groenda, Sebastian Lehrig
  */
 public class CloseDataSourceHandler extends AbstractHandler implements IHandler {
@@ -25,12 +30,21 @@ public class CloseDataSourceHandler extends AbstractHandler implements IHandler 
 
         if (selection instanceof TreeSelection) {
             final TreeSelection treeSelection = (TreeSelection) selection;
-            final Repository repo = (Repository) treeSelection.getFirstElement();
-            try {
-                MeasurementsUtility.ensureClosedRepository(repo);
-                EDP2Plugin.INSTANCE.getRepositories().getAvailableRepositories().remove(repo);
-            } catch (final DataNotAccessibleException e) {
-                // TODO Auto-generated catch block
+            if (treeSelection.getFirstElement() instanceof Repository) {
+                final Repository repo = (Repository) treeSelection.getFirstElement();
+                try {
+                    MeasurementsUtility.ensureClosedRepository(repo);
+                    EDP2Plugin.INSTANCE.getRepositories().getAvailableRepositories().remove(repo);
+                } catch (final DataNotAccessibleException e) {
+                    // TODO Auto-generated catch block
+                }
+            } else {
+                final Shell shell = EDP2UIPlugin.INSTANCE.getWorkbench().getActiveWorkbenchWindow().getShell();
+                ErrorDialog.openError(
+                        shell,
+                        "Select Repository",
+                        "Closing an EDP2 datasource failed.",
+                        new Status(IStatus.ERROR, EDP2UIPlugin.PLUGIN_ID, "You need to select a EDP2 Repository to execute the close command"));
             }
         } else {
             // TODO Error message
